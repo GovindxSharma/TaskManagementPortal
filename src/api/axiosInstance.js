36fee12fae92255a26e884b13d815729f213100b
Trusha2102 <trushababjadeja2003@gmail.com>
2025-11-13 16:09:45 +0530

    configured base url in axiosInstance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL?.trim() ||
+  "https://customercompliance.onrender.com";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
