10dc32e7a5a191a87a99e3da42300d683539903e
Shodhan Shetty <shodhanshetty700@gmail.com>
2026-05-26 11:58:44 +0530

    Revert "Merge branch 'shodhan-dev'"
    This reverts commit 642333ccdad157de5e7f95391c06579eaf85bea5, reversing
    changes made to b945773944fe5a679026f4bbee0405aaf3745292.

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: true
-  },
-
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
